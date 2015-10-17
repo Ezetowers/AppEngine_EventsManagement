1c85500440633f2cdcaf0320f0e05a0668321645
Ezequiel Torres <ezequiel.torresfeyuk@gmail.com>
2015-10-17 02:49:05 -0300

    Ended with the frontend part
    * All the code needs a deep refactor. But for the moment, the priority
      is Jmeter and the Tasks queues

--- guestbook/guestbook.py
+++ guestbook/guestbook.py
@@ -57,14 +57,12 @@ class MainPage(webapp2.RequestHandler):
             'guests_list': guest_list
         }
 
-        # logging.debug(template_values)
-
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
 
 class AddGuest(webapp2.RequestHandler):
-    # @ndb.transactional(retries=3)
+    @ndb.transactional(retries=3)
     def increment_guest_count(self, event_name):
         try:
             events_query = Event.query(ancestor=events_key())
@@ -73,37 +71,43 @@ class AddGuest(webapp2.RequestHandler):
             for event in events:
                 if event.name == event_name:
                     # We found the event. increment the count
+                    logging.debug("Event: " + event_name +\
+                        " - Capacity: " + event.capacity)
+                    logging.debug("Event: " + event_name +\
+                        " - Guests: " + event.guests)
                     if int(event.capacity) > int(event.guests):
-
+                        logging.debug("Event " + event_name +\
+                            ": There is capacity")
                         amount_guests = int(event.guests) + 1
                         event.guests = str(amount_guests)
                         event.put()
-                        break
+                        return True
                     else:
+                        logging.debug("Event " + event_name +\
+                            ": There ISN'T capacity")
                         return False
         except TransactionFailedError:
             # Exit in this case
             sys.exit(-1)
-        finally:
-            return True
 
     def post(self):
         event_name = self.request.get('actualEvent')
 
-        self.increment_guest_count(event_name)
+        if self.increment_guest_count(event_name) == False:
+            self.response.headers['Content-Type'] = "text/plain"
+            self.response.out.write(json.dumps({'add' : 'false'}))
+            return
+        else:
+            # Add the guest
+            guest = Guest(parent=event_guests_key(event_name))
+            guest.name = self.request.get('guestName')
+            guest.surname = self.request.get('guestSurname')
+            guest.email = self.request.get('guestEmail')
+            guest.company = self.request.get('guestCompany')
+            guest.put()
 
-        # Add the guest
-        logging.debug("Actual event: " + event_name)
-
-        guest = Guest(parent=event_guests_key(event_name))
-        guest.name = self.request.get('guestName')
-        guest.surname = self.request.get('guestSurname')
-        guest.email = self.request.get('guestEmail')
-        guest.company = self.request.get('guestCompany')
-        guest.put()
-
-        query_params = {'event_name': event_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
+            self.response.headers['Content-Type'] = "application/json"
+            self.response.out.write(json.dumps({'add' : 'true'}))
 
 
 class EventsCreation(webapp2.RequestHandler):
@@ -125,6 +129,7 @@ class EventsCreation(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/updater', MainPage),
     ('/event_creation', EventsCreation),
     ('/add_guest', AddGuest),
 ], debug=True)
